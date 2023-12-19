03ffabbe2bffac7cac418f22bd2a5b61dec2172f
PETR-ART <ar.petrovpetrov@yandex.ru>
2023-12-19 18:35:32 +0300

    main1

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print('main')
+import pygame
+
+
+def main():
+    pygame.init()
+    size = width, height = 800, 600
+    screen = pygame.display.set_mode(size)
+
+    pygame.display.flip()
+
+    clock = pygame.time.Clock()
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+    pygame.quit()
+
+
+if __name__ == '__main__':
+    main()
