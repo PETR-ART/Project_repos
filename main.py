c6dbacfbc926960169e4e3f5b4d5d7920852f5b6
PETR-ART <ar.petrovpetrov@yandex.ru>
2023-12-23 16:17:27 +0300

    main_window

--- main.py
+++ main.py
@@ -1,11 +1,46 @@
 import pygame
 import sys
+import os
 
 
-def main():
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data/images', name)
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    return image
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+def first():
+    print(1)
+
+
+def start():
+    intro_text = ["ГЕОУГАДАЙКА", "",
+                  "Чтобы начать игру, нажмите пробел",
+                  "текст",
+                  "Для выхода нажмите Escape"]
     pygame.init()
     size = width, height = 800, 600
     screen = pygame.display.set_mode(size)
+    fon = pygame.transform.scale(load_image('fon1.jpg'), (width, height))
+    screen.blit(fon, (0, 0))
+    font = pygame.font.Font(None, 30)
+    text_coord = 50
+    for line in intro_text:
+        string_rendered = font.render(line, 1, pygame.Color('black'))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 10
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
 
     pygame.display.flip()
 
@@ -14,10 +49,15 @@ def main():
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                running = False
+                terminate()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    terminate()
+                if event.key == pygame.K_SPACE:
+                    first(start())
 
     pygame.quit()
 
 
 if __name__ == '__main__':
-    main()
+    start()
